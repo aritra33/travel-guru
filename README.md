7f6f8d733c1b36077d4a2115cbc29d964e75c142
unknown <saritra33@gmail.com>
2020-09-25 21:21:44 +0600

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # travel-guru
+# travel-guru
---
+++ src/Component/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDrlSoF000StiURgGLPZtZwTGMM4-njBMI",
+    authDomain: "travel-guru-d5064.firebaseapp.com",
+    databaseURL: "https://travel-guru-d5064.firebaseio.com",
+    projectId: "travel-guru-d5064",
+    storageBucket: "travel-guru-d5064.appspot.com",
+    messagingSenderId: "868689763267",
+    appId: "1:868689763267:web:6e389065a6dbce7015bb76",
+    measurementId: "G-PF2XWXSC74"
+  };
+  export default firebaseConfig;
